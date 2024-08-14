16693c38efef0847df5792ab5b9c97bcd8e76abc
Rodrigo Alves <rodimportant@gmail.com>
2024-08-14 17:17:41 +0100

    added gitignore finished sync_folders function and main function finalizing tests and adding comments for next version

--- folder_sync.py
+++ folder_sync.py
@@ -22,24 +22,53 @@ def sync_folders(source_path, replica_path, log_file):
         os.makedirs(replica_path)
 
     for root, dirs, files in os.walk(source_path):
-        print(root)
         for dir_name in dirs:
-            print(dir_name)
             dir_path = os.path.join(root, dir_name)
             replica_dir_path = os.path.join(replica_path, os.path.relpath(dir_path, source_path))
+
+            if not os.path.exists(replica_dir_path):
+                log_message(f"Copying directory '{dir_path}' to '{replica_dir_path}'.", log_file)
+                shutil.copytree(dir_path, replica_dir_path)
 
             if not os.path.exists(replica_dir_path):
                 log_message(f"Creating directory '{replica_dir_path}'.", log_file)
                 os.makedirs(replica_dir_path)
 
         for file_name in files:
-            print(file_name)
+            print(files)
             file_path = os.path.join(root, file_name)
             replica_file_path = os.path.join(replica_path, os.path.relpath(file_path, source_path))
+            print(replica_file_path)
 
             if not os.path.exists(replica_file_path):
                 log_message(f"Copying file '{file_path}' to '{replica_file_path}'.", log_file)
                 shutil.copy2(file_path, replica_file_path)
 
+            elif os.path.getmtime(file_path) > os.path.getmtime(replica_file_path):
+                log_message(f"Updating file '{replica_file_path}'.", log_file)
+                shutil.copy2(file_path, replica_file_path)
+
+    log_message(f"Sync complete. {len(os.listdir(source_path))} files and directories copied to '{replica_path}'.",
+                log_file)
 
 
+def main():
+    # This step could have been done with sys.argv, but I preferred to keep it simple and more readable.
+    # with the sys.argv, the command to start the code would have been:
+    # python folder_sync.py <source_path> <replica_path> <sync_interval> <log_file_path>
+
+    source_path = "./source"
+    replica_path = "./replica"
+    sync_interval = 45
+    log_file_path = "./folder_sync.log"
+
+    sync_folders(source_path, replica_path, log_file_path)
+
+    while True:
+        sync_folders(source_path, replica_path, log_file_path)
+        log_message(f"Synchronization complete. Waiting {sync_interval} seconds for next sync.", log_file_path)
+        time.sleep(sync_interval)
+
+
+if __name__ == '__main__':
+    main()
